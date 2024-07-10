2724453126d9d891bddc5725c7a8834b1e98eabc
sayedbasir110 <basir.sayedali@gmail.com>
2024-07-09 18:19:27 -0700

    spartans-selenium-project

---
+++ src/test/java/tek/intro/HandleBrowser.java
@@ -0,0 +1,8 @@
+package tek.intro;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class HandleBrowser {
+    public static void main(String[] args) {
+        ChromeDriver chromeDriver = new ChromeDriver();
+        chromeDriver.get("http://facebook.com");
+    }
+}
