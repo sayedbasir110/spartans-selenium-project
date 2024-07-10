ec0b66eeead5e21e61647e3e8cad461658bed262
sayedbasir110 <basir.sayedali@gmail.com>
2024-07-09 18:32:46 -0700

    spartans-selenium-project

--- src/test/java/tek/intro/NavigateToFacebook.java
+++ src/test/java/tek/intro/NavigateToFacebook.java
@@ -5,6 +5,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class NavigateToFacebook {
     public static void main(String[] args) {
         ChromeDriver chromeDriver = new ChromeDriver();
+        // navigate to google.com
         chromeDriver.get("http://facebook.com");
+        // to maximize the browser window
+        chromeDriver.manage().window().maximize();
+        // close the browser entirely
+        //chromeDriver.quit();
     }
 }
