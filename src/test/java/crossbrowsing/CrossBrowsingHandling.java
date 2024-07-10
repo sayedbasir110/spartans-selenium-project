680cec6eccb49eb5d66451bfa7bddcf97964b2f1
sayedbasir110 <basir.sayedali@gmail.com>
2024-07-10 16:42:33 -0700

    day two

---
+++ src/test/java/crossbrowsing/CrossBrowsingHandling.java
@@ -0,0 +1,26 @@
+package crossbrowsing;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.Scanner;
+
+public class CrossBrowsingHandling {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("What browser do you want to open? ");
+        String browserType;
+        browserType = userInput.next();
+        WebDriver driver;
+        if (browserType.equals("chrome")) driver = new ChromeDriver();
+        else if (browserType.equals("firefox")) driver = new FirefoxDriver();
+        else if (browserType.equals("edge")) driver = new EdgeDriver();
+        else throw new RuntimeException("Wrong driver...");
+
+        driver.get("https://google.com");
+        driver.quit();
+
+    }
+}
