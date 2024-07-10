ad49c8f6f810d2b970a4ae71cf0cf9fd8aad1d93
sayedbasir110 <basir.sayedali@gmail.com>
2024-07-09 18:23:06 -0700

    spartans-selenium-project

--- src/test/java/tek/intro/NavigateToFacebook.java
+++ src/test/java/tek/intro/NavigateToFacebook.java
@@ -1,8 +1,10 @@
 package tek.intro;
+
 import org.openqa.selenium.chrome.ChromeDriver;
-public class HandleBrowser {
+
+public class NavigateToFacebook {
     public static void main(String[] args) {
         ChromeDriver chromeDriver = new ChromeDriver();
-        chromeDriver.get("http://google.com");
+        chromeDriver.get("http://facebook.com");
     }
 }
