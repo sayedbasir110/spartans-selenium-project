804f6cd446b7b878ef3e7fda7c35c187d025a60f
sayedbasir110 <basir.sayedali@gmail.com>
2024-07-18 17:49:49 -0700

    Locators

--- src/test/java/lab_session02/ActivityOne.java
+++ src/test/java/lab_session02/ActivityOne.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.Random;
 
 public class ActivityOne {
     public static void main(String[] args) {
@@ -14,24 +15,26 @@ public class ActivityOne {
 
         driver.manage().window().maximize();
         driver.get("https://retail.tekschool-students.com/");
-        driver.findElement(By.id("signinLink")).click();
-        driver.findElement(By.name("email")).sendKeys("john.doe123@gmail.com");
-        driver.findElement(By.name("password")).sendKeys("John@123");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("signinLink"))).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("email"))).sendKeys("john.doe123@gmail.com");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("password"))).sendKeys("John@123");
         wait.until(ExpectedConditions.elementToBeClickable(By.id("loginBtn"))).click();
-        By element = By.id("accountLink");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(element)).click();
-        driver.findElement(By.id("personalPhoneInput")).clear();
-        driver.findElement(By.id("personalPhoneInput")).sendKeys(String.valueOf(generateRandomPhoneNumber()));
-        driver.findElement(By.id("personalUpdateBtn")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("accountLink"))).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("personalPhoneInput"))).clear();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("personalPhoneInput"))).sendKeys(String.valueOf(generateRandomPhoneNumber()));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("personalUpdateBtn"))).click();
         String messageText = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(),'Personal Information Updated Successfully')]"))).getText();
         System.out.println(messageText);
         driver.quit();
 
     }
-    public static long generateRandomPhoneNumber(){
-        long lowerBound = 1000000000L;
-        long upperBound = 10000000000L;
-        return lowerBound + (long) (Math.random() * (upperBound - lowerBound));
+    public static String generateRandomPhoneNumber(){
+        Random random = new Random();
+        int firstPart = 100 + random.nextInt(900);
+        int secondPart = 100 + random.nextInt(900);
+        int thirdPart = 1000 + random.nextInt(9000);
+        String phoneNumber = firstPart + "-" + secondPart + "-" + thirdPart;
+        return phoneNumber;
     }
 }
